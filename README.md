8aca67ef6788c3db9121d86bef08a98056bf3937
Matthew Hall <mateusvanhalen@gmail.com>
2025-04-21 10:42:25 -0500

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+## http-server
+## node .\Sillyhead.js 
---
+++ Sillyhead.js
@@ -0,0 +1,19 @@
+
+// Ensure there are no errors
+//console.log("hi my name is Magnolia and I just printed my first code project");
+console.log("400 - 300 =") 
+console.log(400-300);
+//console.log("I did a ton of math tonight with friends and family");
+
+var Magnolia = "Hello my name is Magnolia"
+var FiveYearsOld = "I am five years old"
+var doggie ="ruff ruff ruff ruff"
+var cat = "meow meow meow MEOW!"
+
+//console.log(Magnolia + " and " + FiveYearsOld);
+console.log("A dog says " + doggie)
+console.log("And a kitty cat says "+ cat)
+
+
+// node .\Sillyhead.js 
+// to run code
